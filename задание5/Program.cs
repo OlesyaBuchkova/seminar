fb1d7618bc59187796774f30a52741f6dfb63287
olesya <nearolesya@gmil.com>
2023-08-22 21:28:19 +0300

    задание5

---
+++ задание5/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите координаты первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+//double d = Math.Pow(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2), 0.5);
+
+// double d = Math.Pow((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)), 0.5);
+
+// double d = Math.Sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+
+Console.WriteLine(d);
